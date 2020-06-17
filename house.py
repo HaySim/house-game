88b1028bb3f519cd60ef29b475b1319b057e064b
HaySim <hayley_simmons@outlook.com>
2020-06-17 01:20:15 +0100

    first commit

---
+++ house.py
@@ -0,0 +1,17 @@
+# Defining functions:
+
+# def square(x):
+#     x_squared = x**2
+#     return x_squared
+
+
+# while loops and for-loops:
+
+# print(square(8))
+# n = 0
+# while n < 3:
+#     n = n + 1
+#     print(n)
+
+# for n in range(0, 4):
+#     print(n)
