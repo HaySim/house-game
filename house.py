d4469581c44192a23efd400ae2c9eaabd67cd831
HaySim <hayley_simmons@outlook.com>
2020-06-19 00:37:10 +0100

    prototyping with tkinter

--- house.py
+++ house.py
@@ -1,17 +1,28 @@
-# Defining functions:
+import tkinter as tk
 
-# def square(x):
-#     x_squared = x**2
-#     return x_squared
+def callback():
+    print("Clicked")
+
+# Create a new window object and storing its reference in root variable:
+root = tk.Tk()
+root.title("House Game")
+
+# Create a canvas of a fixed size attached to root:
+canvas = tk.Canvas(root, width = 1500, height = 900)
+canvas.pack()
+
+# Store id of text object:
+txt_id = canvas.create_text(250,150, font="Times 100", text="_ _ _ _ _")
+
+# Rename text value:
+canvas.itemconfig(txt_id, text = "Hi") 
 
 
-# while loops and for-loops:
 
-# print(square(8))
-# n = 0
-# while n < 3:
-#     n = n + 1
-#     print(n)
+canvas.create_line(0, 0, 100, 100)
 
-# for n in range(0, 4):
-#     print(n)
+btn = tk.Button(master = root, command = callback, text = "A", font = "Times 100")
+btn.place(x = 30 , y = 600)
+
+
+root.mainloop()
