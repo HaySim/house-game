c9dd90da11f80d283de790871e65f6beb0b4976d
HaySim <hayley_simmons@outlook.com>
2020-06-19 21:14:35 +0100

    implement drawing house

--- house.py
+++ house.py
@@ -34,6 +34,31 @@ def place_buttons():
             on_second_row = True
 
 
+# Creating the shapes for the house (x1, y1, x2, y2):
+def draw_house(life):
+    # Draw large rectangle (main body of house): 
+    if life == 1:
+        canvas.create_rectangle(155, 250, 580, 635, fill = "", width = 3)
+    # Draw triangle (for roof): 
+    elif life == 2:
+        points = [155, 250, 580, 250, 367, 100, 155, 250]
+        canvas.create_polygon(points, fill = "", outline = "black",  width = 3)
+    # Draw rectangle (for door)
+    elif life == 3:
+        canvas.create_rectangle(330, 470, 404, 635, fill = "", width = 3)
+    # Draw rectangle (top left window):
+    elif life == 4:
+        canvas.create_rectangle(193, 300, 293, 400, width = 3)
+    # Draw rectangle (top right window):
+    elif life == 5:
+        canvas.create_rectangle(442, 300, 542, 400, width = 3)
+    # Draw rectangle (bottom left window):
+    elif life == 6:
+        canvas.create_rectangle(193, 500, 293, 600, width = 3)
+    # Draw rectangle (bottom right window):
+    elif life == 7:
+        canvas.create_rectangle(442, 500, 542, 600, width = 3)
+
 # Create a new window object and storing its reference in root variable:
 root = tk.Tk()
 root.title("House Game")
@@ -43,17 +68,23 @@ canvas = tk.Canvas(root, width = 1650, height = 950)
 canvas.pack()
 
 # Store id of text object:
-txt_id = canvas.create_text(250,150, font = "Times 100", text = "_ _ _ _ _")
+txt_id = canvas.create_text(1000,150, font = "Times 120", text = "_ _ _ _ _")
 
 # Rename text value:
-canvas.itemconfig(txt_id, text = "Hi") 
+#canvas.itemconfig(txt_id, text = "Hi") 
+
+
+draw_house(1)
+draw_house(2)
+draw_house(3)
+draw_house(4)
+draw_house(5)
+draw_house(6)
+draw_house(7)
 
 
 
-canvas.create_line(0, 0, 100, 100)
 
 place_buttons()
-
-
 
 root.mainloop()
